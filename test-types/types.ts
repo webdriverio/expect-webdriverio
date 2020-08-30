b8c4be0a72d69dd6788c49596c85445b3fb89a02
Mykola Grybyk <mykola.grybyk@gmail.com>
2020-08-30 13:35:41 +0200

    fix .not types

--- test-types/types.ts
+++ test-types/types.ts
@@ -1,3 +1,4 @@
 expect({}).toBeDisabled()
 expect({}).toHaveAttr('test')
+expect({}).not.toHaveAttr('test')
 expect('foo').toBe('bar')
--- types/standalone.d.ts
+++ types/standalone.d.ts
@@ -1,7 +1,11 @@
 /// <reference types="expect-webdriverio/types/expect-webdriverio"/>
 
 declare namespace ExpectWebdriverIO {
-    interface Matchers <R,T> extends Readonly<import('expect/build/types').Matchers<R>> {}
+    interface Matchers <R,T> extends Readonly<import('expect/build/types').Matchers<R>> {
+        not: Matchers <R,T>
+        resolves: Matchers<Promise<R>, T>
+        rejects: Matchers<Promise<R>, T>
+    }
     type Expect = {
         <T = unknown>(actual: T): Matchers<T, T>;
     }
