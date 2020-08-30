6e9d25f4fff1c99a9e493504e8c64e09ddfb6b41
Mykola Grybyk <mykola.grybyk@gmail.com>
2020-08-30 18:59:16 +0200

    rename conflicting matchers

--- test-types/types.ts
+++ test-types/types.ts
@@ -1,3 +1,7 @@
 expect({}).toBeDisabled()
 expect({}).toHaveAttr('test')
 expect('foo').toBe('bar')
+
+expect('foo').toHaveElementClass('bar')
+expect('foo').toHaveElementClassContaining('bar')
+expect('foo').toHaveElementProperty('n', 'v', {})
