0c3ca5552f0753d096fa45119e498659ced42135
Mike Grybyk <mykola.grybyk@servicehouse.nl>
2020-09-12 20:53:02 +0200

    fix toBeRequested message

--- src/matchers/mock/toBeRequestedTimes.ts
+++ src/matchers/mock/toBeRequestedTimes.ts
@@ -2,13 +2,20 @@ import { waitUntil, enhanceError, compareNumbers } from '../../utils'
 import { runExpect } from '../../util/expectAdapter'
 import { numberError } from '../../util/formatMessage'
 
-function toBeRequestedTimesFn(received: WebdriverIO.Mock, times: number | ExpectWebdriverIO.NumberOptions = {}, options: ExpectWebdriverIO.CommandOptions = {}): any {
+export function toBeRequestedTimesFn(received: WebdriverIO.Mock, times: number | ExpectWebdriverIO.NumberOptions = {}, options: ExpectWebdriverIO.NumberOptions = {}): any {
     const isNot = this.isNot || false
     const { expectation = `called${typeof times === 'number' ? ' ' + times : '' } time${times !== 1 ? 's' : ''}`, verb = 'be' } = this
 
-    const eq = typeof times === 'number' ? times : times.eq
-    const gte = typeof times === 'number' ? 1 : (times.gte || 1)
-    const lte = typeof times === 'number' ? 0 : (times.lte || 0)
+    // type check
+    if (typeof times === 'number') {
+        options.eq = times
+    } else if (typeof times === 'object') {
+        options = { ...options, ...times }
+    }
+
+    const eq = options.eq
+    const gte = options.gte || 1
+    const lte = options.lte || 0
 
     return browser.call(async () => {
         let actual
@@ -16,7 +23,7 @@ function toBeRequestedTimesFn(received: WebdriverIO.Mock, times: number | Expect
         const pass = await waitUntil(async () => {
             actual = received.calls.length
             return compareNumbers(actual, gte, lte, eq)
-        }, isNot, options)
+        }, isNot, { ...options, wait: isNot ? 0 : options.wait })
 
         const error = numberError(gte, lte, eq)
         const message = enhanceError('mock', error, actual, this, verb, expectation, '', options)
