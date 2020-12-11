3b87b642ab83ab78e2409bd8373f5adbafaa4bce
Mike Grybyk <mykola.grybyk@servicehouse.nl>
2020-12-11 14:59:12 +0100

    downgrade ts version to 3.8.3

--- test-types/jasmine/expectAsync.ts
+++ test-types/jasmine/expectAsync.ts
@@ -1,11 +1,5 @@
 declare const promise: Promise<object>
 
 it('should do something', () => {
-    // Promises shouldn't have the `foo` method.
-    expectAsync(promise)
-        .toBeDisplayed()
-        // @ts-expect-error
-        .foo()
-
     expectAsync(promise).toBeDisplayed().then()
 })
