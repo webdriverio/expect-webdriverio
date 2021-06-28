081a12fab56936389577eb6a793d2ac74ab4f7e2
TuxedoFish <harryliversedge@gmail.com>
2021-06-28 17:18:50 +0100

    add remaining tests for enhanceError

--- test/util/formatMessage.test.ts
+++ test/util/formatMessage.test.ts
@@ -1,4 +1,4 @@
-import { printDiffOrStringify } from 'jest-matcher-utils';
+import { printDiffOrStringify, printExpected, printReceived } from 'jest-matcher-utils';
 
 import { enhanceError } from '../../src/util/formatMessage'
 
@@ -21,13 +21,67 @@ describe('formatMessage', () => {
             })
 
             test('starting message', () => {
-                expect(actual.includes('Expect Test Subject to Test Verb Test Expectation')).toBe(true)
+                expect(actual).toMatch('Expect Test Subject to Test Verb Test Expectation')
             })
 
             test('diff string', () => {
                 let diffString = printDiffOrStringify('Test Expected', 'Test Actual', 'Expected', 'Received', true)
-                expect(actual.includes(diffString)).toBe(true)
+                expect(actual).toMatch(diffString)
             })
+        })
+
+        describe('isNot', () => {
+            let actual: string
+
+            describe('different', () => {
+                beforeEach(() => {
+                    actual = enhanceError(
+                        'Test Subject',
+                        'Test Expected',
+                        'Test Actual',
+                        { isNot: true },
+                        'Test Verb',
+                        'Test Expectation',
+                        '',
+                        { message: '', containing: false }
+                    )
+                })
+
+                test('starting message', () => {
+                    expect(actual).toMatch("Expect Test Subject not to Test Verb Test Expectation")
+                })
+
+                test('diff string', () => {
+                    let diffString = printDiffOrStringify('Test Expected', 'Test Actual', 'Expected [not]', 'Received      ', true)
+                    expect(actual).toMatch(diffString)
+                })
+            })
+
+            describe('same', () => {
+                beforeEach(() => {
+                    actual = enhanceError(
+                        'Test Subject',
+                        'Test Same',
+                        'Test Same',
+                        { isNot: true },
+                        'Test Verb',
+                        'Test Expectation',
+                        '',
+                        { message: '', containing: false }
+                    )
+                })
+
+                test('starting message', () => {
+                    expect(actual).toMatch("Expect Test Subject not to Test Verb Test Expectation")
+                })
+
+                test('diff string', () => {
+                    let diffString = `Expected [not]: ${printExpected("Test Same")}\n` +
+                        `Received      : ${printReceived("Test Same")}`
+                    expect(actual).toMatch(diffString)
+                })
+            })
+
         })
 
         describe('containing', () => {
@@ -47,12 +101,64 @@ describe('formatMessage', () => {
             })
 
             test('starting message', () => {
-                expect(actual.includes('Expect Test Subject to Test Verb Test Expectation containing')).toBe(true)
+                expect(actual).toMatch('Expect Test Subject to Test Verb Test Expectation containing')
             })
 
             test('diff string', () => {
                 let diffString = printDiffOrStringify('Test Expected', 'Test Actual', 'Expected', 'Received', true)
-                expect(actual.includes(diffString)).toBe(true)
+                expect(actual).toMatch(diffString)
+            })
+        })
+
+        describe('message', () => {
+            let actual: string
+
+            beforeEach(() => {
+                actual = enhanceError(
+                    'Test Subject',
+                    'Test Expected',
+                    'Test Actual',
+                    { isNot: false },
+                    'Test Verb',
+                    'Test Expectation',
+                    '',
+                    { message: 'Test Message', containing: false }
+                )
+            })
+
+            test('starting message', () => {
+                expect(actual).toMatch('Test Message\nExpect Test Subject to Test Verb Test Expectation')
+            })
+
+            test('diff string', () => {
+                let diffString = printDiffOrStringify('Test Expected', 'Test Actual', 'Expected', 'Received', true)
+                expect(actual).toMatch(diffString)
+            })
+        })
+
+        describe('arg2', () => {
+            let actual: string
+
+            beforeEach(() => {
+                actual = enhanceError(
+                    'Test Subject',
+                    'Test Expected',
+                    'Test Actual',
+                    { isNot: false },
+                    'Test Verb',
+                    'Test Expectation',
+                    'Test Arg2',
+                    { message: 'Test Message', containing: false }
+                )
+            })
+
+            test('starting message', () => {
+                expect(actual).toMatch('Expect Test Subject to Test Verb Test Expectation Test Arg2')
+            })
+
+            test('diff string', () => {
+                let diffString = printDiffOrStringify('Test Expected', 'Test Actual', 'Expected', 'Received', true)
+                expect(actual).toMatch(diffString)
             })
         })
     })
