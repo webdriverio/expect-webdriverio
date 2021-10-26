42805e02b98a23789fc89ed2ac49ec87888521af
christian-bromann <mail@christian-bromann.com>
2021-10-26 11:09:24 +0200

    note that types are now automatically embedded, see webdriverio/webdriverio#7604

--- types/standalone-global.d.ts
+++ types/standalone-global.d.ts
@@ -1,5 +1,8 @@
 /// <reference types="expect-webdriverio/types/standalone"/>
 
+/**
+ * to use as an additional expectation lib (not recommended)
+ */
 declare const expectWdio: ExpectWebdriverIO.Expect
 
 declare namespace NodeJS {
