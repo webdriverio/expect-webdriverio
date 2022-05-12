73daf48185a8b4fefd02f8c04250e9448206975c
Chris Plummer <strmer15@users.noreply.github.com>
2022-05-12 18:57:27 -0400

    update dependencies and fix expect import (#754)

--- types/standalone.d.ts
+++ types/standalone.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="expect-webdriverio/types/expect-webdriverio"/>
 
 declare namespace ExpectWebdriverIO {
-    interface Matchers<R, T> extends Readonly<import('expect/build/types').Matchers<R>> {
+    interface Matchers<R, T> extends Readonly<import('expect').Matchers<R>> {
         not: Matchers<R, T>
         resolves: Matchers<Promise<R>, T>
         rejects: Matchers<Promise<R>, T>
