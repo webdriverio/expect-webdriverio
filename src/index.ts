5a531cfacaf0de884ffa914ab319c118dd694dab
Christian Bromann <git@bromann.dev>
2024-02-05 16:19:31 -0800

    (feat): keep track of registered matchers

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 /// <reference types="../types/standalone.d.ts" />
 import { expect as expectLib } from 'expect'
+import type { RawMatcherFn } from './types.js'
 
 import wdioMatchers from './matchers.js'
 import { DEFAULT_OPTIONS } from './constants.js'
 
-expectLib.extend({ ...wdioMatchers })
+export const matchers = new Map<string, RawMatcherFn>()
+
+const extend = expectLib.extend
+expectLib.extend = (m) => {
+    if (!m || typeof m !== 'object') {
+        return
+    }
+
+    Object.entries(m).forEach(([name, matcher]) => matchers.set(name, matcher))
+    return extend(m)
+}
+
+expectLib.extend(wdioMatchers)
 export const expect = expectLib as unknown as ExpectWebdriverIO.Expect
-export const matchers = wdioMatchers
 export const getConfig = (): any => DEFAULT_OPTIONS
 export const setDefaultOptions = (options = {}): void => {
     Object.entries(options).forEach(([key, value]) => {
