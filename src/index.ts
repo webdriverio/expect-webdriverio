87a4b0a9dd24a127534e05e0fa3dcd3a09a5b214
Erwin Heitzman <15839059+erwinheitzman@users.noreply.github.com>
2024-02-12 15:41:07 +0100

    expect-webdriverio: revert breaking change

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,13 @@
 /// <reference types="../types/standalone.d.ts" />
 import { expect as expectLib } from 'expect'
-import type { RawMatcherFn } from './types.js'
 
 import wdioMatchers from './matchers.js'
 import { DEFAULT_OPTIONS } from './constants.js'
 
-export const matchers = new Map<string, RawMatcherFn>()
+expectLib.extend({ ...wdioMatchers })
 
-const extend = expectLib.extend
-expectLib.extend = (m) => {
-    if (!m || typeof m !== 'object') {
-        return
-    }
-
-    Object.entries(m).forEach(([name, matcher]) => matchers.set(name, matcher))
-    return extend(m)
-}
-
-expectLib.extend(wdioMatchers)
 export const expect = expectLib as unknown as ExpectWebdriverIO.Expect
+export const matchers = wdioMatchers
 export const getConfig = (): any => DEFAULT_OPTIONS
 export const setDefaultOptions = (options = {}): void => {
     Object.entries(options).forEach(([key, value]) => {
--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,3 @@
-import type { ExpectationResult, MatcherContext } from 'expect'
-
 export type WdioElementMaybePromise =
     WebdriverIO.Element | WebdriverIO.ElementArray |
     Promise<WebdriverIO.Element> | Promise<WebdriverIO.ElementArray>
-
-export type RawMatcherFn<Context extends MatcherContext = MatcherContext> = {
-    (this: Context, actual: any, ...expected: Array<any>): ExpectationResult;
-}
