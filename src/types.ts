28c2ed710e34cdf04754e475f48c5ad27f664520
Christian Bromann <git@bromann.dev>
2024-02-12 08:02:48 -0800

    Revert "expect-webdriverio: revert breaking change"
    This reverts commit 87a4b0a9dd24a127534e05e0fa3dcd3a09a5b214.

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,9 @@
+import type { ExpectationResult, MatcherContext } from 'expect'
+
 export type WdioElementMaybePromise =
     WebdriverIO.Element | WebdriverIO.ElementArray |
     Promise<WebdriverIO.Element> | Promise<WebdriverIO.ElementArray>
+
+export type RawMatcherFn<Context extends MatcherContext = MatcherContext> = {
+    (this: Context, actual: any, ...expected: Array<any>): ExpectationResult;
+}
--- test/matchers.test.ts
+++ test/matchers.test.ts
@@ -1,64 +1,74 @@
-import { test, expect } from 'vitest'
-import Matchers from '../src/matchers.js'
+import { test, expect, vi } from 'vitest'
+import { matchers, expect as expectLib } from '../src/index.js'
+
+const ALL_MATCHERS = [
+    // browser
+    'toHaveClipboardText',
+    'toHaveClipboardTextContaining',
+    'toHaveTitle',
+    'toHaveTitleContaining',
+    'toHaveUrl',
+    'toHaveUrlContaining',
+
+    // elements
+    'toBeElementsArrayOfSize',
+
+    // elements
+    'toBeClickable',
+    'toBeDisabled',
+    'toBeDisplayed',
+    'toBeDisplayedInViewport',
+    'toBeEnabled',
+    'toExist',
+    'toBeExisting',
+    'toBePresent',
+    'toBeFocused',
+    'toBeSelected',
+    'toBeChecked',
+    'toHaveAttributeAndValue',
+    'toHaveAttribute',
+    'toHaveAttributeContaining',
+    'toHaveAttrContaining',
+    'toHaveAttr',
+    'toHaveChildren',
+    'toHaveClass',
+    'toHaveElementClass',
+    'toHaveElementClassContaining',
+    'toHaveClassContaining',
+    'toHaveHref',
+    'toHaveLink',
+    'toHaveHrefContaining',
+    'toHaveLinkContaining',
+    'toHaveHTML',
+    'toHaveHTMLContaining',
+    'toHaveId',
+    'toHaveSize',
+    'toHaveElementProperty',
+    'toHaveText',
+    'toHaveTextContaining',
+    'toHaveValue',
+    'toHaveValueContaining',
+    'toHaveStyle',
+
+    // mock
+    'toBeRequested',
+    'toBeRequestedTimes',
+    'toBeRequestedWith',
+    'toBeRequestedWithResponse',
+
+    // snapshot
+    'toMatchSnapshot',
+    'toMatchInlineSnapshot'
+]
 
 test('matchers', () => {
-    expect(Object.keys(Matchers)).toEqual([
-        // browser
-        'toHaveClipboardText',
-        'toHaveClipboardTextContaining',
-        'toHaveTitle',
-        'toHaveTitleContaining',
-        'toHaveUrl',
-        'toHaveUrlContaining',
+    expect([...matchers.keys()]).toEqual(ALL_MATCHERS)
+})
 
-        // elements
-        'toBeElementsArrayOfSize',
-
-        // elements
-        'toBeClickable',
-        'toBeDisabled',
-        'toBeDisplayed',
-        'toBeDisplayedInViewport',
-        'toBeEnabled',
-        'toExist',
-        'toBeExisting',
-        'toBePresent',
-        'toBeFocused',
-        'toBeSelected',
-        'toBeChecked',
-        'toHaveAttributeAndValue',
-        'toHaveAttribute',
-        'toHaveAttributeContaining',
-        'toHaveAttrContaining',
-        'toHaveAttr',
-        'toHaveChildren',
-        'toHaveClass',
-        'toHaveElementClass',
-        'toHaveElementClassContaining',
-        'toHaveClassContaining',
-        'toHaveHref',
-        'toHaveLink',
-        'toHaveHrefContaining',
-        'toHaveLinkContaining',
-        'toHaveHTML',
-        'toHaveHTMLContaining',
-        'toHaveId',
-        'toHaveSize',
-        'toHaveElementProperty',
-        'toHaveText',
-        'toHaveTextContaining',
-        'toHaveValue',
-        'toHaveValueContaining',
-        'toHaveStyle',
-
-        // mock
-        'toBeRequested',
-        'toBeRequestedTimes',
-        'toBeRequestedWith',
-        'toBeRequestedWithResponse',
-
-        // snapshot
-        'toMatchSnapshot',
-        'toMatchInlineSnapshot'
-    ])
+test('allows to add matcher', () => {
+    const matcher: any = vi.fn((actual: any, expected: any) => ({ pass: actual === expected }))
+    expectLib.extend({ toBeCustom: matcher })
+    // @ts-expect-error not in types
+    expectLib('foo').toBeCustom('foo')
+    expect(matchers.keys()).toContain('toBeCustom')
 })
