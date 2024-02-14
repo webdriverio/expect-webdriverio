99d8a2e8fae90af902bd45800fcee054b6f0a5d9
Christian Bromann <git@bromann.dev>
2024-02-13 17:26:00 -0800

    Revert "(fix): remove obsolete type that clashes with Jest"
    This reverts commit 2eac1de04fe7c7172adcfda3de421f63a8560e7d.

--- types/jest-global.d.ts
+++ types/jest-global.d.ts
@@ -1,5 +1,8 @@
 /// <reference types="./standalone.js"/>
 
+// @ts-expect-error
+declare const expect: ExpectWebdriverIO.Expect
+
 declare namespace NodeJS {
     interface Global {
         expect: ExpectWebdriverIO.Expect;
