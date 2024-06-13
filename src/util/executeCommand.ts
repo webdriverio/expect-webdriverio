35203f74b3fd92d02aedc3607878679e0c5ec973
Sang Nguyen <ngcongsang@gmail.com>
2024-06-13 22:46:20 +0700

    update JSDoc

--- src/util/executeCommand.ts
+++ src/util/executeCommand.ts
@@ -1,10 +1,9 @@
 /**
  * Ensures that the specified condition passes for a given element or every element in an array of elements
- * @param {WebdriverIO.Element | WebdriverIO.ElementArray} el The element or array of elements
- * @param {(el: WebdriverIO.Element | WebdriverIO.ElementArray, ...params: any[]) => Promise<{ result: boolean; value?: any; }>} condition - The condition function to be executed on the element(s). It should return a promise that resolves to an object containing a result boolean and an optional value
- * @param {ExpectWebdriverIO.DefaultOptions} [options={}] - Optional configuration options
- * @param {any[]} [params=[]] - Additional parameters
- * @returns {Promise<{ el: WebdriverIO.Element | WebdriverIO.ElementArray; success: boolean; values: any }>} - An object containing the element(s), a success boolean indicating if the condition passed, and the values returned from the condition function
+ * @param el The element or array of elements
+ * @param condition - The condition function to be executed on the element(s).
+ * @param options - Optional configuration options
+ * @param params - Additional parameters
  */
 export async function executeCommand(
     el: WebdriverIO.Element | WebdriverIO.ElementArray,
