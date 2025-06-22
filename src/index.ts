5b0875b4c8fe7db4ded1ede83c8d6118428f3fdf
David Prevost <david.prevost@goto.com>
2025-06-21 21:58:19 -0400

    Working case with only matchers

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ import type { RawMatcherFn } from './types.js'
 
 import * as wdioMatchers from './matchers.js'
 import { DEFAULT_OPTIONS } from './constants.js'
-import createSoftExpect from './softExpect.js'
-import { SoftAssertService } from './softAssert.js'
 
 export const matchers = new Map<string, RawMatcherFn>()
 const filteredMatchers = {}
@@ -31,27 +29,6 @@ type MatchersObject = Parameters<typeof expectLib.extend>[0]
 
 expectLib.extend(filteredMatchers as MatchersObject)
 
-// Extend the expect object with soft assertions
-const expectWithSoft = expectLib as unknown as ExpectWebdriverIO.Expect
-Object.defineProperty(expectWithSoft, 'soft', {
-    value: <T = unknown>(actual: T) => createSoftExpect(actual)
-})
-
-// Add soft assertions utility methods
-Object.defineProperty(expectWithSoft, 'getSoftFailures', {
-    value: (testId?: string) => SoftAssertService.getInstance().getFailures(testId)
-})
-
-Object.defineProperty(expectWithSoft, 'assertSoftFailures', {
-    value: (testId?: string) => SoftAssertService.getInstance().assertNoFailures(testId)
-})
-
-Object.defineProperty(expectWithSoft, 'clearSoftFailures', {
-    value: (testId?: string) => SoftAssertService.getInstance().clearFailures(testId)
-})
-
-export const expect = expectWithSoft
-
 export const getConfig = (): ExpectWebdriverIO.DefaultOptions => DEFAULT_OPTIONS
 export const setDefaultOptions = (options = {}): void => {
     Object.entries(options).forEach(([key, value]) => {
@@ -62,17 +39,6 @@ export const setDefaultOptions = (options = {}): void => {
     })
 }
 export const setOptions = setDefaultOptions
-
-/**
- * export snapshot utilities
- */
-export { SnapshotService } from './snapshot.js'
-
-/**
- * export soft assertion utilities
- */
-export { SoftAssertService } from './softAssert.js'
-export { SoftAssertionService, type SoftAssertionServiceOptions } from './softAssertService.js'
 
 /**
  * export utils
