172f9df33856a11a0e78fae6668ddc20fac84a85
David Prevost <david.prevost@goto.com>
2025-06-21 23:35:02 -0400

    Block toHaveUrl on element

--- jest.d.ts
+++ jest.d.ts
@@ -1,14 +1,11 @@
 /// <reference types="./types/expect-webdriverio.d.ts"/>
 
 declare namespace jest {
-    // noinspection JSUnusedGlobalSymbols
-    interface Matchers<R> extends CustomMatchers<R>{}
 
-    // noinspection JSUnusedGlobalSymbols
+    interface Matchers<R, T> extends CustomMatchers<R, T>{}
 
-    interface Expect extends CustomMatchers {}
-
-    // noinspection JSUnusedGlobalSymbols
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    interface Expect extends CustomMatchers<any> {}
 
     interface InverseAsymmetricMatchers extends Expect {}
 }
