544ff14f50514353a7bb99d39d9b2007fb406b04
David Prevost <david.prevost@goto.com>
2025-06-23 01:08:47 -0400

    Add decent not finished typing for Jasmine + soft assertion

---
+++ types/jasmine-soft-extend.d.ts
@@ -0,0 +1,30 @@
+export {}
+
+declare global {
+    function expect<T>(actual: T): jasmine.Matchers<T>
+    namespace expect {
+
+        /**
+         * Creates a soft assertion wrapper around standard expect
+         * Soft assertions record failures but don't throw errors immediately
+         * All failures are collected and reported at the end of the test
+         */
+        function soft<T>(actual: T): jasmine.Matchers<T>
+        // soft<T = unknown>(actual: T): T extends PromiseLikeType ? Matchers<Promise<void>, T> : Matchers<void, T>
+
+        /**
+         * Get all current soft assertion failures
+         */
+        function getSoftFailures(testId?: string): WebdriverIO.SoftFailure[]
+
+        /**
+         * Manually assert all soft failures (throws an error if any failures exist)
+         */
+        function assertSoftFailures(testId?: string): void
+
+        /**
+         * Clear all current soft assertion failures
+         */
+        function clearSoftFailures(testId?: string): void
+    }
+}
