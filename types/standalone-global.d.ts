4479d0bd6e99ff74858c2070dbfa122abdd5aea1
David Prevost <david.prevost@goto.com>
2025-06-27 18:39:57 -0400

    Move new mocha test to jest, review + simplify jest.d.ts

--- types/standalone-global.d.ts
+++ types/standalone-global.d.ts
@@ -1,5 +1,10 @@
 /// <reference types="./expect-webdriverio.d.ts"/>
 
+/**
+ * Global declaration file for WebdriverIO's Expect library when not pair with another expect library like Jest. or Jasmine.
+ * One example is mocha without the chai expect library.
+ */
+
 // We override the existing one, probably coming from `types/jest`
 // @ts-expect-error
 declare const expect: ExpectWebdriverIO.Expect
