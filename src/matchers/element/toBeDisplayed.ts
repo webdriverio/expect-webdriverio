2df2ff6d88c960f53d9e761e1f4cb7a9a0786bcf
Erwin Heitzman <15839059+erwinheitzman@users.noreply.github.com>
2025-07-22 23:35:34 +0200

    polish(expect-webdriverio): expose contentVisibilityAuto, opacityProperty and visibilityProperty to toBeDisplayed

--- src/matchers/element/toBeDisplayed.ts
+++ src/matchers/element/toBeDisplayed.ts
@@ -2,10 +2,38 @@ import { executeCommandBe } from '../../utils.js'
 import { DEFAULT_OPTIONS } from '../../constants.js'
 import type { WdioElementMaybePromise } from '../../types.js'
 
+interface ToBeDisplayedOptions {
+    /**
+     * `true` to check if the element is within the viewport. false by default.
+     */
+    withinViewport?: boolean
+    /**
+     * `true` to check if the element content-visibility property has (or inherits) the value auto,
+     * and it is currently skipping its rendering. `true` by default.
+     * @default true
+     */
+    contentVisibilityAuto?: boolean
+    /**
+     * `true` to check if the element opacity property has (or inherits) a value of 0. `true` by default.
+     * @default true
+     */
+    opacityProperty?: boolean
+    /**
+     * `true` to check if the element is invisible due to the value of its visibility property. `true` by default.
+     * @default true
+     */
+    visibilityProperty?: boolean
+}
+
 export async function toBeDisplayed(
     received: WdioElementMaybePromise,
-    options: ExpectWebdriverIO.CommandOptions = DEFAULT_OPTIONS
+    options: ExpectWebdriverIO.CommandOptions & ToBeDisplayedOptions = DEFAULT_OPTIONS
 ) {
+    options.withinViewport ??= false;
+    options.contentVisibilityAuto ??= true;
+    options.opacityProperty ??= true;
+    options.visibilityProperty ??= true;
+
     this.expectation = this.expectation || 'displayed'
 
     await options.beforeAssertion?.({
