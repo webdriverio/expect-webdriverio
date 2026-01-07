2a1626a2d75fa6cc3e3f4215373c4f8fef616e7f
dprevost-perso <dprevost-github@outlook.com>
2026-01-07 18:49:52 -0500

    Remove unnecessary fixture code

--- test/__fixtures__/beforeAll.js
+++
@@ -1,7 +0,0 @@
-beforeAll(() => {
-    // default options
-    if (expect._expectWebdriverio) {
-        expect._expectWebdriverio.options.wait = 50
-        expect._expectWebdriverio.options.interval = 10
-    }
-})
