6084cac19a7de325c11a6e3ba08da27b634eed3f
dprevost-perso <dprevost-github@outlook.com>
2026-01-26 18:38:09 -0500

    Code review

--- test/types.test-d.ts
+++ test/types.test-d.ts
@@ -6,54 +6,57 @@ import { expect as expectWdio } from '../src/index.js'
 
 describe('Type test', () => {
 
-    test('Jest "expect" lib type tests as baseline', () => {
+    describe('Expects + Promise matchers & basic matchers', () => {
+
+        test('Jest "expect" lib type tests as baseline', () => {
+            // Basic matchers
+            expectTypeOf(expectLib(true)).toExtend<Matchers<void, boolean> & Inverse<Matchers<void, boolean>>>()
+            expectTypeOf(expectLib(true).toBe(true)).toBeVoid()
+            expectTypeOf(expectLib(true).toBe(true)).not.toExtend<Promise<void>>()
+            expectTypeOf(expectLib(Promise.resolve(true)).toBe(expect.any)).toBeVoid()
+            expectTypeOf(expectLib(Promise.resolve(true)).resolves.toBe(expect.any)).resolves.toBeVoid()
+
+            // element matchers are not available in 'expect' lib
+            expectTypeOf(expectLib($('element')).toBe(expect.any)).toBeVoid()
+            expectTypeOf(expectLib($('element'))).not.toHaveProperty('toHaveText')
+            expectTypeOf(expectLib($$('elements')).toBe(expect.any)).toBeVoid()
+            expectTypeOf(expectLib($$('elements'))).not.toHaveProperty('toHaveText')
+        })
+
+        test('Wdio expect & matchers type tests', () => {
         // Basic matchers
-        expectTypeOf(expectLib(true)).toExtend<Matchers<void, boolean> & Inverse<Matchers<void, boolean>>>()
-        expectTypeOf(expectLib(true).toBe(true)).toBeVoid()
-        expectTypeOf(expectLib(true).toBe(true)).not.toExtend<Promise<void>>()
-        expectTypeOf(expectLib(Promise.resolve(true)).toBe(expect.any)).toBeVoid()
-        expectTypeOf(expectLib(Promise.resolve(true)).resolves.toBe(expect.any)).resolves.toBeVoid()
+            expectTypeOf(expectWdio(true)).toExtend<Matchers<void, boolean> & Inverse<Matchers<void, boolean>>>()
+            expectTypeOf(expectWdio(true).toBe(true)).toBeVoid()
+            expectTypeOf(expectWdio(true).toBe(true)).not.toExtend<Promise<void>>()
+            expectTypeOf(expectWdio(Promise.resolve(true)).toBe(true))
+            expectTypeOf(expectWdio(Promise.resolve(true)).resolves.toBe(true)).resolves.toBeVoid()
 
-        // element matchers are not available in 'expect' lib
-        expectTypeOf(expectLib($('element')).toBe(expect.any)).toBeVoid()
-        expectTypeOf(expectLib($('element'))).not.toHaveProperty('toHaveText')
-        expectTypeOf(expectLib($$('elements')).toBe(expect.any)).toBeVoid()
-        expectTypeOf(expectLib($$('elements'))).not.toHaveProperty('toHaveText')
-    })
+            // element matchers
+            expectTypeOf(expectWdio($('element')).toBe(expect.any)).toBeVoid()
+            expectTypeOf(expectWdio($('element')).toHaveText('test')).not.toBeVoid()
+            expectTypeOf(expectWdio($('element')).toHaveText('test')).toExtend<Promise<void>>()
+            expectTypeOf(expectWdio($$('elements')).toBe(expect.any)).toBeVoid()
+            expectTypeOf(expectWdio($$('elements')).toHaveText('test')).toExtend<Promise<void>>()
+            expectTypeOf(expectWdio($$('elements')).toHaveText('test')).not.toBeVoid()
 
-    test('Wdio expect & matchers type tests', () => {
+        })
+
+        test('Wdio soft expect & matchers type tests', () => {
         // Basic matchers
-        expectTypeOf(expectWdio(true)).toExtend<Matchers<void, boolean> & Inverse<Matchers<void, boolean>>>()
-        expectTypeOf(expectWdio(true).toBe(true)).toBeVoid()
-        expectTypeOf(expectWdio(true).toBe(true)).not.toExtend<Promise<void>>()
-        expectTypeOf(expectWdio(Promise.resolve(true)).toBe(true))
-        expectTypeOf(expectWdio(Promise.resolve(true)).resolves.toBe(true)).resolves.toBeVoid()
+            expectTypeOf(expectWdio.soft(true)).toExtend<Matchers<void, boolean> & Inverse<Matchers<void, boolean>>>()
+            expectTypeOf(expectWdio.soft(true).toBe(true)).toExtend<void>()
+            expectTypeOf(expectWdio.soft(true).toBe(true)).not.toExtend<Promise<void>>()
+            // TODO to fix one day? When non elements matchers + promise, we should stick to void and not have Promise<void>
+            //expectTypeOf(expectWdio.soft(Promise.resolve(true)).toBe(expect.any)).toBeVoid()
+            expectTypeOf(expectWdio.soft(Promise.resolve(true)).resolves.toBe(expect.any)).toExtend<Promise<void>>()
 
-        // element matchers
-        expectTypeOf(expectWdio($('element')).toBe(expect.any)).toBeVoid()
-        expectTypeOf(expectWdio($('element')).toHaveText('test')).not.toBeVoid()
-        expectTypeOf(expectWdio($('element')).toHaveText('test')).toExtend<Promise<void>>()
-        expectTypeOf(expectWdio($$('elements')).toBe(expect.any)).toBeVoid()
-        expectTypeOf(expectWdio($$('elements')).toHaveText('test')).toExtend<Promise<void>>()
-        expectTypeOf(expectWdio($$('elements')).toHaveText('test')).not.toBeVoid()
-
-    })
-
-    test('Wdio soft expect & matchers type tests', () => {
-        // Basic matchers
-        expectTypeOf(expectWdio.soft(true)).toExtend<Matchers<void, boolean> & Inverse<Matchers<void, boolean>>>()
-        expectTypeOf(expectWdio.soft(true).toBe(true)).toExtend<void>()
-        expectTypeOf(expectWdio.soft(true).toBe(true)).not.toExtend<Promise<void>>()
-        // to fix?
-        //expectTypeOf(expectWdio.soft(Promise.resolve(true)).toBe(expect.any)).toBeVoid()
-        expectTypeOf(expectWdio.soft(Promise.resolve(true)).resolves.toBe(expect.any)).toExtend<Promise<void>>()
-
-        // element matchers
-        expectTypeOf(expectWdio($('element')).toBe(expect.any)).toBeVoid()
-        expectTypeOf(expectWdio($('element')).toHaveText('test')).not.toBeVoid()
-        expectTypeOf(expectWdio($('element')).toHaveText('test')).toExtend<Promise<void>>()
-        expectTypeOf(expectWdio($$('elements')).toBe(expect.any)).toBeVoid()
-        expectTypeOf(expectWdio($$('elements')).toHaveText('test')).toExtend<Promise<void>>()
-        expectTypeOf(expectWdio($$('elements')).toHaveText('test')).not.toBeVoid()
+            // element matchers
+            expectTypeOf(expectWdio($('element')).toBe(expect.any)).toBeVoid()
+            expectTypeOf(expectWdio($('element')).toHaveText('test')).not.toBeVoid()
+            expectTypeOf(expectWdio($('element')).toHaveText('test')).toExtend<Promise<void>>()
+            expectTypeOf(expectWdio($$('elements')).toBe(expect.any)).toBeVoid()
+            expectTypeOf(expectWdio($$('elements')).toHaveText('test')).toExtend<Promise<void>>()
+            expectTypeOf(expectWdio($$('elements')).toHaveText('test')).not.toBeVoid()
+        })
     })
 })
