5a62ef3d757517502de1dbe27bf3cc5fa3dacf49
dprevost-perso <dprevost-github@outlook.com>
2026-01-27 07:52:45 -0500

    Add mocha playgrounds
    - expect-wdio releases breaks easily other integration since we just test with unit tests
    - Adding playground will help testing further before releasing and also helps troubleshooting existing problem inside the project

---
+++ playgrounds/mocha/wdio.conf.ts
@@ -0,0 +1,63 @@
+
+import { SoftAssertionService, setOptions } from 'expect-webdriverio'
+
+export const config: WebdriverIO.Config = {
+    //
+    // ====================
+    // Runner Configuration
+    // ====================
+    //
+    runner: 'local',
+
+    //
+    // ==================
+    // Specify Test Files
+    // ==================
+    //
+    specs: [
+        './test/specs/**/*.test.ts'
+    ],
+
+    //
+    // ============
+    // Capabilities
+    // ============
+    //
+    maxInstances: 10,
+    capabilities: [{
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: ['--headless', '--disable-gpu']
+        }
+    }],
+
+    //
+    // ===================
+    // Test Configurations
+    // ===================
+    //
+    logLevel: 'info',
+    bail: 0,
+    baseUrl: 'http://localhost',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    services: [
+        [SoftAssertionService, {}]
+    ],
+    framework: 'mocha',
+    reporters: ['spec'],
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    },
+
+    //
+    // =====
+    // Hooks
+    // =====
+    //
+    before: function (_capabilities, _specs) {
+        setOptions({ wait: 250 })
+    },
+}
