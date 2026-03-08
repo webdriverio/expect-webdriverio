8041f404f03f84dc89212dc9797574043324d78e
dprevost-perso <dprevost-github@outlook.com>
2026-03-08 09:51:29 -0400

    Better integrated tests

--- playgrounds/mocha/test/specs/options.test.ts
+++ playgrounds/mocha/test/specs/options.test.ts
@@ -2,29 +2,30 @@ import { $ } from '@wdio/globals'
 import { setOptions, getConfig } from 'expect-webdriverio'
 
 describe('Global Options', () => {
+    const defaultWait = getConfig().wait
 
     before(() => {
-        // Verify we can set the option to 1ms
         setOptions({ wait: 1 })
+    })
+
+    it('should set global wait option', () => {
         expect(getConfig().wait).toBe(1)
+        expect(getConfig().wait).not.toBe(defaultWait)
+        expect(defaultWait).toBe(10000)
     })
 
     it('should allow setting and using global wait option', async () => {
         const start = Date.now()
 
-        // This should fail almost immediately because the element doesn't exist
-        // and the wait time is set to 1ms
+        // Should fail immediately (wait: 1ms)
         await expect(expect($('non-existent-element-' + Date.now())).toBeDisplayed()).rejects.toThrow()
         const duration = Date.now() - start
 
-        // It should take significantly less time than the default or the 250ms set in wdio.conf
-        // 1ms wait + overhead. 500ms should be a safe upper bound for "immediate" failure
-        // vs a standard 3000ms timeout
+        // Ensure failure was fast (< 500ms) compared to default timeout
         expect(duration).toBeLessThan(500)
     })
 
     after(() => {
-        // Restore the option to 250ms as inferred from wdio.conf.ts to not pollute other tests
-        setOptions({ wait: 250 })
+        setOptions({ wait: defaultWait })
     })
 })
