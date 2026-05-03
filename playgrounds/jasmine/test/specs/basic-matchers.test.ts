24008860335745d348b9ea69adda403fffe77a36
dprevost-perso <dprevost-github@outlook.com>
2026-05-03 06:36:23 -0400

    fix attribute that can be null in playgrounds

--- playgrounds/jasmine/test/specs/basic-matchers.test.ts
+++ playgrounds/jasmine/test/specs/basic-matchers.test.ts
@@ -52,7 +52,8 @@ describe('Basic Expect Matchers', () => {
             const navLinks = await $$('nav a')
             const hrefs: string[] = []
             for (const link of navLinks) {
-                hrefs.push(await link.getAttribute('href'))
+                const href = await link.getAttribute('href')
+                if(href) hrefs.push(href)
             }
 
             await expect(hrefs).toBeInstanceOf(Array)
