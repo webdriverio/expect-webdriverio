a5c7e6abc7b4921304afb72c9884ecdea404d1ce
dprevost-perso <dprevost-github@outlook.com>
2026-06-06 21:59:49 -0400

    fix any

--- playgrounds/jest/jest.setup.after-env.ts
+++ playgrounds/jest/jest.setup.after-env.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { jest, beforeAll, afterAll, expect } from "@jest/globals";
 import { remote } from "webdriverio";
 import { config } from "./wdio.conf";
@@ -9,7 +8,7 @@ jest.setTimeout(30000);
 beforeAll(async () => {
     // Add custom wdio matcher to Jest's expect
     // TODO should MatchersObject type from Jest's expect lib. Fix incoming.
-    // @disable-eslint-next-line @typescript-eslint/no-explicit-any -- Fix incoming
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any -- Fix incoming
     expect.extend(matchers as any);
 
     globalThis.standalone = await remote(config);
