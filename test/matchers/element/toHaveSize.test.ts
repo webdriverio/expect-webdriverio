b13d46250935cfe69a7685054229742d1425d527
David Prevost <77302423+dprevost-LMI@users.noreply.github.com>
2026-07-01 15:34:39 -0400

    chore: refactor toHaveSize + tests as a preparation for $$ support (#2131)
    * Refactor toHaveSize + tests as a preparation for $$ support
    
    * Fix Greptile concerns

--- test/matchers/element/toHaveSize.test.ts
+++ test/matchers/element/toHaveSize.test.ts
@@ -1,115 +1,144 @@
-import { vi, test, describe, expect } from 'vitest'
+import { vi, test, describe, expect, beforeEach } from 'vitest'
 import { $ } from '@wdio/globals'
 
+import type { Size } from '../../../src/matchers/element/toHaveSize.js'
 import { toHaveSize } from '../../../src/matchers/element/toHaveSize.js'
+import stripAnsi from 'strip-ansi'
 
 vi.mock('@wdio/globals')
 
-describe('toHaveSize', () => {
-    test('wait for success', async () => {
-        const el = await $('sel')
-        el.getSize = vi.fn().mockResolvedValue({ width: 32, height: 32 })
-        const beforeAssertion = vi.fn()
-        const afterAssertion = vi.fn()
+describe(toHaveSize, async () => {
+    let thisContext: { toHaveSize: typeof toHaveSize }
+    let thisNotContext: { isNot: true; toHaveSize: typeof toHaveSize }
 
-        const result = await toHaveSize.call({}, el, { width: 32, height: 32 }, { beforeAssertion, afterAssertion })
+    const expectedValue: Size = { width: 32, height: 32 }
+    const wrongValue: Size = { width: 15, height: 32 }
 
-        expect(result.pass).toBe(true)
-        expect(el.getSize).toHaveBeenCalledTimes(1)
-        expect(beforeAssertion).toBeCalledWith({
-            matcherName: 'toHaveSize',
-            expectedValue: { width: 32, height: 32 },
-            options: { beforeAssertion, afterAssertion }
+    beforeEach(() => {
+        thisContext =  { toHaveSize }
+        thisNotContext = { isNot: true, ...thisContext }
+    })
+
+    describe.for([
+        { element: await $('sel'), type: 'awaited ChainablePromiseElement' },
+        { element: await $('sel').getElement(), type: 'awaited getElement of ChainablePromiseElement (e.g. WebdriverIO.Element)' },
+        { element: $('sel'), type: 'non-awaited of ChainablePromiseElement' }
+    ])('given a single element when $type', ({ element }) => {
+        let el: ChainablePromiseElement | WebdriverIO.Element
+
+        beforeEach(() => {
+            el = element
+            vi.mocked(el.getSize).mockResolvedValue(expectedValue as unknown as Size & number) // vitest does not support overloads function well
         })
-        expect(afterAssertion).toBeCalledWith({
-            matcherName: 'toHaveSize',
-            expectedValue: { width: 32, height: 32 },
-            options: { beforeAssertion, afterAssertion },
-            result
+
+        test('wait for success', async () => {
+            const beforeAssertion = vi.fn()
+            const afterAssertion = vi.fn()
+
+            const result = await thisContext.toHaveSize(el, expectedValue, { beforeAssertion, afterAssertion, wait: 500 })
+
+            expect(result.pass).toBe(true)
+            expect(el.getSize).toHaveBeenCalledTimes(1)
+            expect(beforeAssertion).toHaveBeenCalledWith({
+                matcherName: 'toHaveSize',
+                expectedValue: expectedValue,
+                options: { beforeAssertion, afterAssertion, wait: 500 }
+            })
+            expect(afterAssertion).toHaveBeenCalledWith({
+                matcherName: 'toHaveSize',
+                expectedValue: expectedValue,
+                options: { beforeAssertion, afterAssertion, wait: 500 },
+                result
+            })
         })
-    })
 
-    test('wait but failure', async () => {
-        const el = await $('sel')
-        el.getSize = vi.fn().mockRejectedValue(new Error('some error'))
+        test('wait but error', async () => {
+            vi.mocked(el.getSize).mockRejectedValue(new Error('some error'))
 
-        await expect(() => toHaveSize.call({}, el, { width: 32, height: 32 }, {}))
-            .rejects.toThrow('some error')
-    })
+            await expect(() => thisContext.toHaveSize(el, expectedValue, { wait: 500 }))
+                .rejects.toThrow('some error')
+        })
 
-    test('success on the first attempt', async () => {
-        const el = await $('sel')
-        el.getSize = vi.fn().mockResolvedValue({ width: 32, height: 32 })
+        test('success by default', async () => {
+            const result = await thisContext.toHaveSize(el, expectedValue)
 
-        const result = await toHaveSize.call({}, el, { width: 32, height: 32 }, {})
+            expect(result.pass).toBe(true)
+            expect(el.getSize).toHaveBeenCalledTimes(1)
+        })
 
-        expect(result.pass).toBe(true)
-        expect(el.getSize).toHaveBeenCalledTimes(1)
-    })
+        test('no wait - failure with proper error message', async () => {
+            vi.mocked(el.getSize).mockResolvedValue(wrongValue as unknown as Size & number)
 
-    test('no wait - failure', async () => {
-        const el = await $('sel')
-        el.getSize = vi.fn().mockResolvedValue({ width: 32, height: 32 })
+            const result = await thisContext.toHaveSize(el, expectedValue, { wait: 0 })
 
-        const result = await toHaveSize.call({}, el, { width: 15, height: 32 }, { wait: 0 })
+            expect(result.pass).toBe(false)
+            expect(el.getSize).toHaveBeenCalledTimes(1)
+            expect(stripAnsi(result.message())).toEqual(`\
+Expect $(\`sel\`) to have size
 
-        expect(result.pass).toBe(false)
-        expect(el.getSize).toHaveBeenCalledTimes(1)
-    })
+- Expected  - 1
++ Received  + 1
 
-    test('no wait - success', async () => {
-        const el = await $('sel')
-        el.getSize = vi.fn().mockResolvedValue({ width: 32, height: 32 })
+  Object {
+    "height": 32,
+-   "width": 32,
++   "width": 15,
+  }`
+            )
+        })
 
-        const result = await toHaveSize.call({}, el, { width: 32, height: 32 }, { wait: 0 })
+        test('no wait - success', async () => {
+            const result = await thisContext.toHaveSize(el, expectedValue, { wait: 0 })
 
-        expect(result.pass).toBe(true)
-        expect(el.getSize).toHaveBeenCalledTimes(1)
-    })
+            expect(result.pass).toBe(true)
+            expect(el.getSize).toHaveBeenCalledTimes(1)
+        })
 
-    test('not - failure - pass should be true', async () => {
-        const el = await $('sel')
-        el.getSize = vi.fn().mockResolvedValue({ width: 32, height: 32 })
-        const result = await toHaveSize.call({ isNot: true }, el, { width: 32, height: 32 }, { wait: 0 })
+        test('not - failure - pass should be true', async () => {
+            const result = await thisNotContext.toHaveSize(el, expectedValue)
 
-        expect(result.pass).toBe(true) // failure, boolean is inverted later because of `.not`
-        expect(result.message()).toEqual(`\
+            expect(result.pass).toBe(true) // failure, boolean is inverted later because of `.not`
+            expect(stripAnsi(result.message())).toEqual(`\
 Expect $(\`sel\`) not to have size
 
 Expected [not]: {"height": 32, "width": 32}
 Received      : {"height": 32, "width": 32}`
-        )
-    })
+            )
+        })
 
-    test("should return false if sizes don't match", async () => {
-        const el = await $('sel')
-        el.getSize = vi.fn().mockResolvedValue({ width: 32, height: 32 })
+        test('should fails with custom failure message', async () => {
+            vi.mocked(el.getSize).mockResolvedValue(wrongValue as unknown as Size & number)
 
-        const result = await toHaveSize.bind({})(el, { width: 15, height: 32 }, { wait: 1 })
+            const result = await thisContext.toHaveSize(el, expectedValue, { wait: 1, message: 'Custom error message' })
 
-        expect(result.pass).toBe(false)
-    })
-
-    test('should return true if sizes match', async () => {
-        const el = await $('sel')
-        el.getSize = vi.fn().mockResolvedValue({ width: 32, height: 32 })
-
-        const result = await toHaveSize.bind({})(el, { width: 32, height: 32 }, { wait: 1 })
-
-        expect(result.pass).toBe(true)
-    })
-
-    test('message', async () => {
-        const el = await $('sel')
-        el.getSize = vi.fn().mockResolvedValue(null)
-
-        const result = await toHaveSize.call({}, el, { width: 32, height: 32 })
-
-        expect(result.pass).toBe(false)
-        expect(result.message()).toEqual(`\
+            expect(result.pass).toBe(false)
+            expect(stripAnsi(result.message())).toEqual(`\
+Custom error message
 Expect $(\`sel\`) to have size
 
-Expected: {"height": 32, "width": 32}
-Received: null`)
+- Expected  - 1
++ Received  + 1
+
+  Object {
+    "height": 32,
+-   "width": 32,
++   "width": 15,
+  }`
+            )
+        })
+
+        // TODO bring back with PR supporting $$
+        test.skip('should fails when expected is an unsupported array type', async () => {
+            const result = await thisContext.toHaveSize(el, [expectedValue] as any)
+
+            expect(result.pass).toBe(false)
+            expect(stripAnsi(result.message())).toEqual(`\
+Expect $(\`sel\`) to have size
+
+Expected: [{"height": 32, "width": 32}]
+Received: "Expected value cannot be an array"`
+            )
+        })
+
     })
 })
