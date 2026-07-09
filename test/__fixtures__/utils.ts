8e1b5811d5ee24019d916c94ac516143e9eeaaff
dprevost-perso <dprevost-github@outlook.com>
2026-07-09 06:51:22 -0400

    Unused fixture

--- test/__fixtures__/utils.ts
+++ test/__fixtures__/utils.ts
@@ -2,19 +2,3 @@ export function matcherNameLastWords(matcherName: string) {
     return matcherName.replace(/^toHave/, '').replace(/^toBe/, '')
         .replace(/([A-Z])/g, ' $1').trim().toLowerCase()
 }
-
-export function getExpectMessage(msg: string) {
-    return msg.split('\n')[0]
-}
-
-export function getExpected(msg: string) {
-    return getReceivedOrExpected(msg, 'Expected')
-}
-
-export function getReceived(msg: string) {
-    return getReceivedOrExpected(msg, 'Received')
-}
-
-function getReceivedOrExpected(msg: string, type: string) {
-    return msg.split('\n').find((line, idx) => idx > 1 && line.startsWith(type))
-}
