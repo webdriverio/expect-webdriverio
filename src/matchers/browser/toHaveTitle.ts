7ce3821606fb24c437a9af3bf8524b6c9c5ba924
dprevost-perso <dprevost-github@outlook.com>
2026-08-03 07:25:22 -0400

    POC multiRemote

--- src/matchers/browser/toHaveTitle.ts
+++ src/matchers/browser/toHaveTitle.ts
@@ -1,9 +1,22 @@
-import { waitUntil, enhanceError, compareText } from '../../utils.js'
+import { waitUntil, enhanceError, compareText, isAsymmetricMatcher } from '../../utils.js'
 import { DEFAULT_OPTIONS } from '../../constants.js'
+import type { StrategyResult } from '../../util/executeCommand.js'
 
 export async function toHaveTitle(
     browser: WebdriverIO.Browser,
     expectedValue: string | RegExp | AsymmetricMatcher<string>,
+    options?: ExpectWebdriverIO.StringOptions
+): Promise<ExpectWebdriverIO.AssertionResult>
+
+export async function toHaveTitle(
+    browser: WebdriverIO.MultiRemoteBrowser,
+    expectedValue: MaybeArrayOrMultiRemoteValues<string | RegExp | AsymmetricMatcher<string>>,
+    options: ExpectWebdriverIO.StringOptions
+): Promise<ExpectWebdriverIO.AssertionResult>
+
+export async function toHaveTitle(
+    browser: WebdriverIO.Browser | WebdriverIO.MultiRemoteBrowser,
+    expectedValue: MaybeArrayOrMultiRemoteValues<string | RegExp | AsymmetricMatcher<string>>,
     options: ExpectWebdriverIO.StringOptions = DEFAULT_OPTIONS
 ) {
     const { expectation = 'title', verb = 'have', isNot, matcherName = 'toHaveTitle' } = this
@@ -16,11 +29,14 @@ export async function toHaveTitle(
 
     const { actual, success } = await waitUntil(
         async () => {
-            const actual = await browser.getTitle()
 
-            const compareResult = compareText(actual, expectedValue, options)
-
-            return { actual, success: compareResult.success, subject: browser }
+            if (browser.isMultiremote) {
+                return await compareMultiRemoteTitles(browser, expectedValue, options)
+            } else if (typeof expectedValue === 'object' || Array.isArray(expectedValue) && !isAsymmetricMatcher(expectedValue)) {
+                throw new Error('Expected value object or array is not supported for a single browser instance. Use a string, RegExp or asymmetric matcher instead.')
+            } else {
+                return await compareBrowserTitle(browser, expectedValue, options)
+            }
         },
         isNot,
         { wait: options.wait, interval: options.interval }
@@ -41,3 +57,46 @@ export async function toHaveTitle(
 
     return result
 }
+
+const compareBrowserTitle = async (
+    browser: WebdriverIO.Browser,
+    expectedValue: string | RegExp | AsymmetricMatcher<string>,
+    options: ExpectWebdriverIO.StringOptions
+): Promise<StrategyResult<string>> => {
+    const actual = await browser.getTitle()
+    const result = compareText(actual, expectedValue, options)
+    return { actual: result.actual, success: result.success, subject: browser }
+}
+
+const compareMultiRemoteTitles = async (
+    browser: WebdriverIO.MultiRemoteBrowser,
+    expectedValue: MaybeArrayOrMultiRemoteValues<string | RegExp | AsymmetricMatcher<string>>,
+    options: ExpectWebdriverIO.StringOptions
+): Promise<StrategyResult<MaybeArray<string>>> => {
+    let browserNames: string[] | undefined
+    let expectedValues: MaybeArray<string | RegExp | AsymmetricMatcher<string>> | undefined
+
+    if (typeof expectedValue === 'object') {
+        browserNames = Object.keys(expectedValue)
+        if (browserNames.length === 0) {
+            throw new Error('Expected value object is empty. Please provide at least one browser instance name with its expected title.')
+        }
+
+        expectedValues = Object.values(expectedValue)
+    } else {
+        expectedValues = expectedValue
+    }
+    const actual = await (browserNames ? browser.select(browserNames) : browser).getTitle()
+
+    if (Array.isArray(actual)){
+        const results = actual.map((title, index) => compareText(title, Array.isArray(expectedValues) ? expectedValues[index] : expectedValues, options))
+        return { actual: results.map(r => r.actual), success: results.every(r => r.success), subject: browser }
+    } else if (Array.isArray(expectedValues)) {
+        throw new Error('Expected value is an array but actual value is not. Please provide a single expected value for a single browser instance.')
+    }
+
+    const result = compareText(actual, expectedValues, options)
+    return { ...result, subject: browser }
+
+}
+
