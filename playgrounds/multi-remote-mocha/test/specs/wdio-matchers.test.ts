611ad3128d75a44097eceed5b547abff5a5fd97a
dprevost-perso <dprevost-github@outlook.com>
2026-08-27 15:40:55 -0400

    Add better array test case

--- playgrounds/multi-remote-mocha/test/specs/wdio-matchers.test.ts
+++ playgrounds/multi-remote-mocha/test/specs/wdio-matchers.test.ts
@@ -285,10 +285,19 @@ describe('WebdriverIO Custom Matchers', () => {
                 })
 
                 it('should verify element have text with array of expect values', async () => {
+                    // Setting different values to test if the order of the array matters
+                    await multiRemoteBrowser.getInstance('firefox').execute(() => {
+                        const h1 = document.querySelector('h1')
+                        if (h1) h1.textContent = 'WebdriverJS Testpage Firefox'
+                    })
+                    await multiRemoteBrowser.getInstance('chrome').execute(() => {
+                        const h1 = document.querySelector('h1')
+                        if (h1) h1.textContent = 'WebdriverJS Testpage Chrome'
+                    })
                     const h1 = multiRemoteBrowser.$('h1')
 
-                    // TODO dprevost: Instance order is not garantee therefore the below would be unstable. So we remove support?
-                    await expect(h1).toHaveText(['WebdriverJS Testpage', 'WebdriverJS Testpage'])
+                    // TODO dprevost: Is instances order is garanteed?
+                    await expect(h1).toHaveText(['WebdriverJS Testpage Chrome', 'WebdriverJS Testpage Firefox'])
                 })
 
                 it('should verify element have text with multi-remote expect values', async () => {
